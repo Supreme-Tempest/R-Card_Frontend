6f2df1c60286929cfda814d14b203880d81e31f2
CristianAlfaro <00167917@uca.edu.sv>
2021-07-04 22:33:32 -0600

    Signup and login fixed

--- public/javascripts/user/signup.js
+++ public/javascripts/user/signup.js
@@ -1,5 +1,40 @@
 var form = (document.forms.signup);
-console.log(form);
+
+
+function verificarPasswords() {
+ 
+    // Ontenemos los valores de los campos de contraseñas 
+    pass1 = document.getElementsByName('password');
+    pass2 = document.getElementsByName('validated_password');
+ 
+    // Verificamos si las constraseñas no coinciden 
+    if (pass1.value != pass2.value) {
+ 
+        // Si las constraseñas no coinciden mostramos un mensaje 
+        document.getElementById("error").classList.add("mostrar");
+ 
+        return false;
+    } else {
+ 
+        // Si las contraseñas coinciden ocultamos el mensaje de error
+        document.getElementById("error").classList.remove("mostrar");
+ 
+        // Mostramos un mensaje mencionando que las Contraseñas coinciden 
+        document.getElementById("ok").classList.remove("ocultar");
+ 
+        // Desabilitamos el botón de login 
+        document.getElementById("login").disabled = true;
+ 
+        // Refrescamos la página (Simulación de envío del formulario) 
+        setTimeout(function() {
+            location.reload();
+        }, 3000);
+ 
+        return true;
+    }
+ 
+}
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     let data = {
