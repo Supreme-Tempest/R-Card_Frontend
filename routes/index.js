07013221f522875303bddccf3a3e52b605631a70
luist23 <00127717@uca.edu.sv>
2021-07-04 19:43:31 -0600

    view modular

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/testGet', (req, res) => {
 });
 
 router.get('/test', (req, res) => {
-    res.render('diagnostics/Estadisticas')
+    res.render('sales/sales')
 });
 
 module.exports = router;
