4fba6ca55608a5c816a319c10c808b568486daba
CristianAlfaro <00167917@uca.edu.sv>
2021-07-05 08:53:59 -0600

    select option add

--- public/javascripts/user/signup.js
+++ public/javascripts/user/signup.js
@@ -1,62 +1,34 @@
 var form = (document.forms.signup);
+var role = document.getElementById("role");
+console.log(role);
 
-
-function verificarPasswords() {
- 
-    // Ontenemos los valores de los campos de contraseñas 
-    pass1 = document.getElementsByName('password');
-    pass2 = document.getElementsByName('validated_password');
- 
-    // Verificamos si las constraseñas no coinciden 
-    if (pass1.value != pass2.value) {
- 
-        // Si las constraseñas no coinciden mostramos un mensaje 
-        document.getElementById("error").classList.add("mostrar");
- 
-        return false;
-    } else {
- 
-        // Si las contraseñas coinciden ocultamos el mensaje de error
-        document.getElementById("error").classList.remove("mostrar");
- 
-        // Mostramos un mensaje mencionando que las Contraseñas coinciden 
-        document.getElementById("ok").classList.remove("ocultar");
- 
-        // Desabilitamos el botón de login 
-        document.getElementById("login").disabled = true;
- 
-        // Refrescamos la página (Simulación de envío del formulario) 
-        setTimeout(function() {
-            location.reload();
-        }, 3000);
- 
-        return true;
-    }
- 
-}
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    let data = {
-        username: form.username.value,
-        password: form.password.value,
-        name: form.name.value,
-        lastname: form.lastname.value,
-        workshop: 1,
-        role: 2,
-    }
-    fetch('/signup', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(res => res.json())
-        .then(res => {
-            if (res.ok) {
-                console.log("se pudo perro")
-            } else {
-                alert("puede que estes repitiendo nombre o te falten datos");
+    if (form.username.value != "" && form.name.value != "" && form.lastname.value != "" && form.password.value != "") {
+        if (form.password.value == form.validated_password.value) {
+            let data = {
+                username: form.username.value,
+                password: form.password.value,
+                name: form.name.value,
+                lastname: form.lastname.value,
+                workshop: 1,
+                role: parseInt(role.value),
             }
-        })
+            fetch('/signup', {
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json())
+                .then(res => {
+                    if (res.ok) {
+                        console.log("se pudo perro")
+                    } else {
+                        alert("puede que estes repitiendo nombre o te falten datos");
+                    }
+                })
+        }
+    }
 });
