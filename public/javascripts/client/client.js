1f4a52554f354a76804418ab4df8336924c4932b
CristianAlfaro <00167917@uca.edu.sv>
2021-07-08 21:35:05 -0600

    clientePage fix

--- public/javascripts/client/client.js
+++ public/javascripts/client/client.js
@@ -1,13 +1,23 @@
 var table_client = document.getElementById("tbody_client");
 console.log(table_client);
+
+let page = {
+    page: 1,
+    size: 5,
+}
+
 onload();
 
 function onload() { 
-    fetch('/clients/clientpage', {
-        method: 'GET'
+    fetch('/clients/clientPage', {
+        method: 'POST',
+        body: JSON.stringify(page),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }).then(res => res.json()).then(data => {
         let values = "";
-        console.log(data.data.data); 
+        console.log(data); 
         data.data.data.forEach(element => {
             values = values + `
                 <tr>
@@ -18,7 +28,7 @@ function onload() {
                         <button class = "btn btn-danger">Deshabilitar</button>
                     </td>
                 </tr>
-            `
+            ` 
         });
         table_client.innerHTML = values;
     });
