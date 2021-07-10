df410afa43021f34c2ebd0c918d686cd4998ea16
luist23 <00127717@uca.edu.sv>
2021-07-09 21:34:42 -0600

    get data to statistic

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const routes = require('./tools/routes');
+const apiMethods = require('./tools/ApiRequest');
 
 router.get('/stadistics', (req, res) => {
     res.render('diagnostic/stadistics')
 });
+
+router.get('/diagnoticfacture', (req, res) => {
+    apiMethods.ApiGet(routes.diagnosticFacture, req, res);
+}); 
+
+router.get('/diagnoticproduct', (req, res) => {
+    apiMethods.ApiGet(routes.diagnosticProduct, req, res);
+}); 
+
+router.get('/diagnoticproductsaledesc', (req, res) => {
+    apiMethods.ApiGet(routes.diagnosticProductSalDesc, req, res);
+}); 
+
+router.get('/diagnoticproductsaleasc', (req, res) => {
+    apiMethods.ApiGet(routes.diagnosticProductSalAsc, req, res);
+}); 
 
 module.exports = router;
